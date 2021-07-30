d1793c26744c4cd5ed9e17d2c6481249aba37096
mahinwizard4444 <86041088+mahinwizard4444@users.noreply.github.com>
2021-07-31 01:14:11 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -192,8 +192,8 @@ async def delete(bot, message):
 async def bot_info(bot, message):
     buttons = [
         [
-            InlineKeyboardButton('Movie Channel', url='https://t.me/onlymovie76'),
-            InlineKeyboardButton('Master', url='https://t.me/JACKBRO007')
+            InlineKeyboardButton('Update Channel', url='https://onlymovie76'),
+            InlineKeyboardButton('OWNER', url='https://t.me/onlymovie76')
         ]
         ]
-    await message.reply(text="<b>Developer : <a href='https://t.me/Jackbro007'>Jackbro</a>\nLanguage : <code>Python3</code>\nLibrary : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio</a>\nOWNER : <a href='https://t.me/Jackbro007'>Click here</a>\nSeries channel: <a href='https://t.me/onlyseries76'>animation movie group</a> </b>", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
+    await message.reply(text="<b>Developer : <a href='https://t.me/onlymovie76'>ANONYMOUS</a>\nLanguage : <code>Python3</code>\nLibrary : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio</a>\nOWNER : <a href='https://t.me/DKBOTZHELP'>Click here</a>\nUpdate Channel : <a href='https://t.me/onlymovie76'>DK Bots</a> </b>", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
